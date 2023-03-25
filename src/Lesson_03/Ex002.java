f89cb3b2be39ae60d79881da8bd358c047a047e2
Nikitayrf <samaraphone@yandex.ru>
2023-03-25 16:11:07 +0000

    add Lesson_03

---
+++ src/Lesson_03/Ex002.java
@@ -0,0 +1,13 @@
+package Lesson_03;
+import java.util.ArrayList;
+public class Ex002 {
+
+   public static void main(String[] args) {
+       ArrayList<Integer> list = new ArrayList<>();
+       list.add(2809);
+
+       for (Object o : list) {
+           System.out.println(o);
+       }
+   }
+}
