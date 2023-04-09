b557cb6281b89c5b53ffdea1a62fff73f2d9daa2
Nikitayrf <samaraphone@yandex.ru>
2023-04-09 20:43:34 +0000

    add lesson_09

---
+++ src/Lesson_09/Ex004/Program.java
@@ -0,0 +1,15 @@
+package Lesson_09.Ex004;
+
+import Lesson_09.Ex004.ExBeverage.*;
+
+public class Program {
+    public static void main(String[] args) {
+        Coffee latte = new Coffee();
+        latte.addComponent(new Water("вода"));
+        latte.addComponent(new Water("вода"));
+        latte.addComponent(new Water("вода"));
+        for (var ingredient : latte) {
+            System.err.println(ingredient);
+        }
+    }
+}
